4d82db4fdc5ac46b65787e713c9203635e1b5e17
Thomas Kenneth Smith <onintks@googlemail.com>
2013-11-10 12:34:39 +0000

    added screen functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
+var app
+$(function(){
+
+	LOGIN = function(el){
+	    var obj = this;
+	    this.el = $(el);
+
+	    this.init = function(){
+	    	REMOVEOVERLAY(obj, el);
+	    	obj.el.each(function(){
+	    		$(this).on("click", 'a', obj.onSkipClick); 
+	    	});
+
+	    };
+	    this.onSkipClick = function(e){
+	    	e.preventDefault();
+
+	    }
+	    this.init();
+	};
+
+	REMOVEOVERLAY = function(obj, el){
+		var cur = obj.el.find('.overlay').bind('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
+		    function(e) {
+		    	$(this).remove();
+		});
+	}
+
+	INTRO = function(el){
+	    var obj = this;
+	    this.el = $(el);
+
+	    this.init = function(){
+	    	obj.el.each(function(){
+	    		$(this).on("click", 'a', obj.onNavClick); 
+	    	});
+	    };
+	    this.onNavClick = function(e){
+	    	e.preventDefault();
+	    	obj.el.toggleClass('off-screen');
+	    }
+	    this.init();
+	};
+
+
+	app = {
+		init: function(){
+			$(window).load(function(){
+				$('html, body').removeClass('loading');
+				$('#login').removeClass('loading');
+			});
+			app.login = new LOGIN( $('#login') );
+			app.intro = new INTRO( $('#intro') );
+		}
+	}
+	app.init();
+});
 
